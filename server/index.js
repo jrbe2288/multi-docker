8e8f0819b623a59a9e84640ffb6f00f19a1f1d03
jrbe2288 <jrbe228@gmail.com>
2021-04-19 12:30:17 -0400

    corrected diffs from instructor code

--- server/index.js
+++ server/index.js
@@ -19,10 +19,10 @@ const pgClient = new Pool({
   port: keys.pgPort,
 });
 
-pgClient.on('connect', () => {
-  pgClient
+pgClient.on('connect', (client) => {
+  client
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log(err));
+    .catch((err) => console.error(err));
 });
 
 // Redis Client Setup
